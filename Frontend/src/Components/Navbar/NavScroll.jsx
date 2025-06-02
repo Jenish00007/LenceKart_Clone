637f3eb5c7d7fb238e9c5c323be20663b34edcf6
Fenix <fenixt.s2000@gmail.com>
2025-06-02 10:22:08 +0530

    all filters completed

--- Frontend/src/Components/Navbar/NavScroll.jsx
+++ Frontend/src/Components/Navbar/NavScroll.jsx
@@ -7,10 +7,10 @@ const NavScroll = () => {
   const [activeItem, setActiveItem] = useState("EYEGLASSES");
 
   const items = [
-    { name: "EYEGLASSES", icon: <FaGlasses /> },
-    { name: "COMPUTER GLASSES", icon: <FaDesktop /> },
-    { name: "KIDS GLASSES", icon: <FaChild /> },
-    { name: "CONTACT LENSES", icon: <FaEye /> }
+    { name: "EYEGLASSES", icon: <FaGlasses />, mainCategory: "GLASSES", subCategory: "EYEGLASSES"},
+    { name: "COMPUTER GLASSES", icon: <FaDesktop />, mainCategory: "GLASSES", subCategory: "COMPUTER_GLASSES"},
+    { name: "KIDS GLASSES", icon: <FaChild />, mainCategory: "GLASSES", subCategory: "EYEGLASSES", personCategory: "kids" },
+    { name: "CONTACT LENSES", icon: <FaEye />, mainCategory: "CONTACT_LENSES", subCategory: "CONTACT_LENSES"}
   ];
 
   return (
@@ -28,43 +28,53 @@ const NavScroll = () => {
       mt={4}
     >
       <Flex gap={4}>
-        {items.map((item) => (
-          <Link to="/products" key={item.name}>
-            <Flex
-              align="center"
-              position="relative"
-              onClick={() => setActiveItem(item.name)}
-              px={1}
-              gap={2}
+        {items.map((item) => {
+          // Build query string dynamically
+          let query = `mainCategory=${item.mainCategory}&subCategory=${item.subCategory}`;
+          if (item.personCategory) {
+            query += `&personCategory=${item.personCategory}`;
+          }
+          return (
+            <Link
+              to={`/products?${query}`}
+              key={item.name}
             >
-              <Box 
-                fontSize="20px" 
-                color={activeItem === item.name ? "black" : "gray.400"}
+              <Flex
+                align="center"
+                position="relative"
+                onClick={() => setActiveItem(item.name)}
+                px={1}
+                gap={2}
               >
-                {item.icon}
-              </Box>
-              <Text
-                fontSize="12px"
-                fontWeight="500"
-                _hover={{ color: "blue.500" }}
-                minW="max-content"
-                color={activeItem === item.name ? "black" : "gray.400"}
-              >
-                {item.name}
-              </Text>
-              {activeItem === item.name && (
-                <Box
-                  position="absolute"
-                  bottom="-2px"
-                  left="0"
-                  right="0"
-                  height="2px"
-                  bg="black"
-                />
-              )}
-            </Flex>
-          </Link>
-        ))}
+                <Box 
+                  fontSize="20px" 
+                  color={activeItem === item.name ? "black" : "gray.400"}
+                >
+                  {item.icon}
+                </Box>
+                <Text
+                  fontSize="12px"
+                  fontWeight="500"
+                  _hover={{ color: "blue.500" }}
+                  minW="max-content"
+                  color={activeItem === item.name ? "black" : "gray.400"}
+                >
+                  {item.name}
+                </Text>
+                {activeItem === item.name && (
+                  <Box
+                    position="absolute"
+                    bottom="-2px"
+                    left="0"
+                    right="0"
+                    height="2px"
+                    bg="black"
+                  />
+                )}
+              </Flex>
+            </Link>
+          );
+        })}
       </Flex>
     </Box>
   );
